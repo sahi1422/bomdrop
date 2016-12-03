85c3c825c35b62a0042036c96a3cb85c5245217b
Stephen Hensley <stephenhensley@Stephens-MacBook-Pro.local>
2016-12-03 15:38:53 -0800

    convert_test is now working as a stripper to only get the useful rows. From this point, I will need to text replace with the desired Descriptions/Part Numbers/etc.

--- convert_test.py
+++ convert_test.py
@@ -1,3 +1,20 @@
 import csv
 
-with open('
+"""
+with open('input_sample_1.txt') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['Qty'], row['Value'], row['Parts'], row['Description'])
+"""
+
+input_file = 'input_sample_1.txt'
+output_file = 'output_test.txt'
+
+csvinput = open(input_file, 'rb')
+csvoutput = open(output_file, 'wb')
+reader = csv.DictReader(csvinput)
+writer = csv.writer(csvoutput, delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL)
+writer.writerow(['Quantity','Value', 'Parts', 'Description'])
+for row in reader:
+    writer.writerow([row['Qty'], row['Value'], row['Parts'], row['Description']])
+
